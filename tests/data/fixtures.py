15f11264242b7acfceb624e80f76fc1eb9857773
Elizabeth Berrigan <berri104@gmail.com>
2025-01-13 18:03:42 -0800

    move fixture

--- tests/data/fixtures.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import json
-
-@pytest.fixture
-def plantB_day11_json():
-    file_path = 'tests/data/_set1_day1_20230509-125420_014_plantB_day11.json'
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    return data
